e4f7021dcb3bd6b544152ef32a98bdfcddfe75df
everdimension <everdimension@gmail.com>
2021-04-21 16:45:49 +0300

    setup prettier for pre-commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,15 @@ module.exports = {
   arrowParens: 'avoid',
   singleQuote: true,
   trailingComma: 'all',
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        printWidth: 70,
+        trailingComma: 'none',
+        arrowParens: 'avoid',
+        proseWrap: 'never',
+      },
+    },
+  ],
 };
